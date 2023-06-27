53642668413a39bfe31a7ea00ba5d02e935f5f92
kevin-ada <kevinkoech265@gmail.com>
2023-06-27 03:19:52 -0700

    Bubble Sort Implementation

--- sort.h
+++ sort.h
@@ -1,7 +1,23 @@
 #ifndef SORT_H
 #define SORT_H
 
-#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/* macros */
+#define UP 0
+#define DOWN 1
+
+/**
+ * enum bool - Enumeration of Boolean values.
+ * @false: Equals 0.
+ * @true: Equals 1.
+ */
+typedef enum bool
+{
+	false = 0,
+	true
+} bool;
 
 /**
  * struct listint_s - Doubly linked list node
@@ -12,48 +28,18 @@
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
-
-
-/**
- *swap_node - Interchange nodes
- *@node: node
- *@list: node list
- *Return: returns a pointer to anode
- */
-listint_t *swap_node(listint_t *node, listint_t **list)
-{
-	listint_t *back = node->prev, *current = node;
-
-	back->next = current->next;
-	if (current->next)
-		current->next->prev = back;
-	current->next = back;
-	current->prev = back->prev;
-	back->prev = current;
-	if (current->prev)
-		current->prev->next = current;
-	else
-		*list = current;
-	return (current);
-}
-
-
-
-
-/* Function we are supposed to work with */
+/* Needed Functions */
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 
 
-/** Prototypes of the mandatory tasks */
+/* Task Function */
 void bubble_sort(int *array, size_t size);
-void insertion_sort_list(listint_t **list);
-
 
 
 
